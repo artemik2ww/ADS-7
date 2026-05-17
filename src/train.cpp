dc828e49ea3e0943c6a114053d949d7cb3367cd4
Артём <71376415+artemik2ww@users.noreply.github.com>
2026-05-17 17:21:37 +0300

    b125

--- src/train.cpp
+++ src/train.cpp
@@ -22,25 +22,39 @@ void Train::addCar(bool light) {
 int Train::getLength() {
     if (!first) return 0;
     countOp = 0;
-    first->light = true; 
-    Car* current = first;
-    int stepsToCurrent = 0;
+    first->light = true;
+    int stepsToCurrent = 1;
     while (true) {
-        current = current->next;
-        stepsToCurrent++;
-        countOp++;
+        Car* current = first;
+        for (int i = 0; i < stepsToCurrent; i++) {
+            current = current->next;
+            countOp++;
+        }
         if (current->light) {
-            current->light = false; 
-            Car* returnPointer = current;
+            current->light = false;
             for (int i = 0; i < stepsToCurrent; i++) {
-                returnPointer = returnPointer->prev;
+                current = current->prev;
                 countOp++;
             }
-            if (!returnPointer->light) {
+            if (!current->light) {
                 return stepsToCurrent;
             }
+            for (int i = 0; i < stepsToCurrent; i++) {
+                current = current->next;
+                countOp++;
+            }
             current->light = true;
+            for (int i = 0; i < stepsToCurrent; i++) {
+                current = current->prev;
+                countOp++;
+            }
+        } else {
+            for (int i = 0; i < stepsToCurrent; i++) {
+                current = current->prev;
+                countOp++;
+            }
         }
+        stepsToCurrent++;
     }
 }
 
